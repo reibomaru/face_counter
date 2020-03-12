91249d1a06a09d93fe5c1644603074d824f9ad4f
Rei Sugiura <reibomaru@g.ecc.u-tokyo.ac.jp>
2020-03-12 16:49:35 +0900

    edit edit.html and count.js

--- static/count.js
+++ static/count.js
@@ -1,9 +1,11 @@
 const display_face_count = document.getElementById('display_face_count')
 const display_eye_count = document.getElementById('display_eye_count')
+const display_face_only_count = document.getElementById('display_face_only_count')
 const display_time = document.getElementById('display_time')
 let runtime = 0
 let face_count = 0
 let eye_count = 0
+let face_only_count = 0
 
 window.setInterval(refreshHTML, 1000);
 
@@ -18,6 +20,8 @@ function postHandler (json) {
     // console.log(json.face_count)
     face_count = json.face_count
     eye_count = json.eye_count
+    face_only_count = face_count - eye_count
     display_face_count.textContent = face_count
     display_eye_count.textContent = eye_count
+    display_face_only_count.textContent = face_only_count
 }
