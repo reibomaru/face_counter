51b44095078dbd11aa3fe3473245cfeec04d386b
Rei Sugiura <reibomaru@g.ecc.u-tokyo.ac.jp>
2020-03-23 19:04:07 +0900

    adminでCountsを表示させるようにした

--- face_count/models.py
+++ face_count/models.py
@@ -1,1 +1,12 @@
-import 
+from django.conf import settings
+from django.db import models
+
+
+class Count(models.Model):
+    face_count = models.IntegerField()
+    eye_count = models.IntegerField(default=0)
+    start_date = models.DateTimeField(blank=True, null=True)
+    finish_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.face_count
