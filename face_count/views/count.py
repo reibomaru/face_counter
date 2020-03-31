6ae1e629bddbe951ff116b585eddeef5499d57d4
Rei Sugiura <reibomaru@g.ecc.u-tokyo.ac.jp>
2020-03-31 23:35:28 +0900

    イシューを解決

--- face_count/views/count.py
+++ face_count/views/count.py
@@ -1,24 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
 from multiprocessing import Value, Process
 from ctypes import c_bool
+import base64
+import io
+import numpy as np
 import cv2
+from PIL import Image
 import time
 import json
 import os
 from mysite.settings import BASE_DIR
 from face_count.models import Count
 
+count_dict = {
+    'face_count': 0,
+    'eye_count':0
+}
 module_dir = os.path.dirname(__file__)
 json_path = os.path.join(module_dir, 'face_count.json')
 face_count = 0
 eye_count = 0
-isActiveGen = Value(c_bool, False)
+# isActiveGen = Value(c_bool, False)
 face_cascade = cv2.CascadeClassifier(
     BASE_DIR + '/face_count/src/haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(
     BASE_DIR + '/face_count/src/haarcascade_eye.xml')
+# cv2.imread()
 
 
 def index(request):
@@ -26,44 +36,98 @@ def index(request):
 
 
 def count(request):
-    global isActiveGen
-    process = Process(target=gen, args=(isActiveGen,))
-    process.start()
-    isActiveGen.value = True
+    global count_dict
+    count_dict['start_unix_time'] = time.time()
+    # global isActiveGen
+    # process = Process(target=gen, args=(isActiveGen,))
+    # process.start()
+    # isActiveGen.value = True
+    # analize_img()
     return HttpResponse('OK')
 
 
 def stop(request):
-    global isActiveGen
-    isActiveGen.value = False
-    return HttpResponse('OK')
-
-
-def gen(isActiveGen):
-    count_dict = dict()
-    cap_2 = cv2.VideoCapture(0)
-    count_dict['start_unix_time'] = time.time()
-    while isActiveGen.value:
-        _, img = cap_2.read()
-        if None:
-            print('画像がありません')
-            break
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-        if type(faces) != tuple:
-            count_up_face(len(faces))
-            for x, y, w, h in faces:
-                face_gray = gray[y: y + h, x: x + w]
-                eyes = eye_cascade.detectMultiScale(face_gray)
-                if type(eyes) != tuple:
-                    count_up_eye()
-        count_dict['face_count'] = face_count
-        count_dict['eye_count'] = eye_count
-        fw = open(BASE_DIR + '/static/face_count.json', 'w')
-        json.dump(count_dict, fw)
-        time.sleep(3)
+    global count_dict
     count_dict['finish_unix_time'] = time.time()
     record_count(count_dict)
+    # global isActiveGen
+    # isActiveGen.value = False
+    return HttpResponse('OK')
+
+# @csrf_exempt
+def analize_img(request):
+    global count_dict
+    # f = cv2.imdecode(request.body, 0)
+    # print('POST:', request.POST)
+    print('content_type:', request.content_type)
+    # print('body:', request.body)
+    # print('FILES:', request.FILES)
+    # print(request.POST)
+    # request_str = str(request.body)
+    # request_dict = json.loads(request.body)
+    # print(request.POST['img'][:20])
+    request_dict = request.POST
+    img_base64 = request_dict['img']
+    # img_base64 = bytes(img_str)
+    # cap_2 = cv2.VideoCapture(0)
+    # _, img = cap_2.read()
+    # print('ideal:', img.shape)
+    # print('bytes:', img.tobytes())
+    # img_base64 = request.body
+    # print(img_base64)
+    # print(img_base64[:20])
+    r = base64.b64decode(img_base64.replace('data:image/png;base64,', ''))
+                                                       
+    # print(np.frombuffer(img_base))
+    pil_img = Image.open(io.BytesIO(r))
+    img_np = np.asarray(pil_img)
+    # img_np = np.frombuffer(r, dtype=np.uint8)
+    # img_np.reshape([59, 100, 3])
+    img_np = cv2.cvtColor(img_np, cv2.COLOR_RGB2GRAY)
+    # img_np = img_np[:, :, ::-1]
+    cv2.imwrite('python.png', img_np)
+    faces = face_cascade.detectMultiScale(img_np, 1.3, 5)
+    if type(faces) != tuple:
+        count_up_face(len(faces))
+        for x, y, w, h in faces:
+            face_gray = img_np[y: y + h, x: x + w]
+            eyes = eye_cascade.detectMultiScale(face_gray)
+            if type(eyes) != tuple:
+                count_up_eye()
+    # r = base64.decodebytes(img_base64)
+    # q = np.frombuffer(r, dtype=np.float64)
+    # print()
+    # print(img_np.shape)
+    # print(request_dict['width'])
+    # print(request_dict['height'])
+
+    # print(np.frombuffer(img_byte_ideal)
+
+    return JsonResponse(count_dict)
+
+# def gen(isActiveGen):
+#     count_dict = dict()
+#     cap_2 = cv2.VideoCapture(0)
+#     count_dict['start_unix_time'] = time.time()
+#     while isActiveGen.value:
+#         _, img = cap_2.read()
+#         if None:
+#             print('画像がありません')
+#             break
+#         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+#         if type(faces) != tuple:
+#             count_up_face(len(faces))
+#             for x, y, w, h in faces:
+#                 face_gray = gray[y: y + h, x: x + w]
+#                 eyes = eye_cascade.detectMultiScale(face_gray)
+#                 if type(eyes) != tuple:
+#                     count_up_eye()
+#         count_dict['face_count'] = face_count
+#         count_dict['eye_count'] = eye_count
+#         fw = open(BASE_DIR + '/static/face_count.json', 'w')
+#         json.dump(count_dict, fw)
+#         time.sleep(3)
 
 
 def record_count(count):
@@ -78,10 +142,10 @@ def record_count(count):
 
 
 def count_up_face(count):
-    global face_count
-    face_count += count
+    global count_dict
+    count_dict['face_count'] += count
 
 
 def count_up_eye():
-    global eye_count
-    eye_count += 1
+    global count_dict
+    count_dict['eye_count'] += 1
--- static/count.js
+++ static/count.js
@@ -6,6 +6,15 @@ const display_start_time = document.getElementById('display_start_time')
 const count_start_btn = document.getElementById('count_start_btn')
 const count_stop_btn = document.getElementById('count_stop_btn')
 
+const player = document.getElementById('player');
+const snapshotCanvas = document.getElementById('snapshot');
+const captureButton = document.getElementById('capture');
+var videoTracks;
+
+captureButton.addEventListener('click', function () {
+    captureSnapshot()
+});
+
 let face_count_data = {
     face_count: 0,
     eye_count: 0,
@@ -13,12 +22,50 @@ let face_count_data = {
 }
 
 let intervalID = null
-
 const httpRequest = new XMLHttpRequest();
+const csrftoken = getCookie('csrftoken')
 
 document.addEventListener('DOMContentLoaded', function () {
     renderHTMLFromData(face_count_data)
+    navigator.mediaDevices.getUserMedia({ video: { width: { exact: 128 }, height: { exact: 72 }} })
+        .then(handleSuccess);
 })
+
+const handleSuccess = function (stream) {
+    console.log(stream)
+    player.srcObject = stream;
+    videoTracks = stream.getVideoTracks();
+};
+
+function captureSnapshotAndSendImg() {
+    const context = snapshotCanvas.getContext('2d')
+    // document.getElementById('image').src = ''
+    // base_image = new Image();
+    // base_image.src = './japan.png';
+    // base_image.onload = function () {
+    //     context.drawImage(base_image, 0, 0, 128, 72);
+    // }
+    // context.drawImage(document.getElementById('image'), 0, 0);
+    context.drawImage(player, 0, 0, 1280, 720)
+
+    return new Promise(function (resolve, reject) {
+        const imgBlob = snapshotCanvas.toDataURL("image/png", 1.0);
+        console.log(imgBlob.length)
+        // console.log(imgBlob)
+        sendImg(imgBlob).then(
+            function (response) {
+                console.log('画像が正常に送信できています')
+                resolve(response)
+            },
+            function () {
+                console.log('画像が正常に送れないためカウントを終了します。')
+                clearInterval(intervalID)
+                reject()
+            }
+        )
+    })
+    
+}
 
 count_start_btn.addEventListener('click', function () { 
     sendStart().then(
@@ -32,13 +79,14 @@ count_start_btn.addEventListener('click', function () {
     ).then(
         function () {
             intervalID = setInterval(function () {
-                getJSON().then(
+                captureSnapshotAndSendImg().then(
                     function (response) {
+                        console.log(response)
                         renderHTMLFromData(response)
                     },
                     function () {
                         renderHTMLFromData(face_count_data)
-                        alert('カウントを終了します。')
+                        console.log('カウントを終了します。')
                         clearInterval(intervalID)
                     }
                 )
@@ -58,6 +106,7 @@ count_stop_btn.addEventListener('click', function () {
             clearInterval(intervalID)
         }
     )
+    videoTracks.forEach(function (track) { track.stop() });
 })
 
 function sendStart() {
@@ -66,7 +115,7 @@ function sendStart() {
         httpRequest.send()
         httpRequest.onreadystatechange = function () {
             if (httpRequest.readyState === 4) {
-                console.log(httpRequest.status)
+                // console.log(httpRequest.status)
                 if (httpRequest.status === 200) {
                     resolve()
                 } else {
@@ -94,8 +143,24 @@ function sendStop() {
     })
 }
 
-function getJSON() {
+function sendImg(imgBlob) {
     return new Promise(function (resolve, reject) {
+        // const imgBlob = captureSnapshot()
+        // console.log(imgBlob)
+        // json = {
+        //     // img: imgBlob,
+        //     height: snapshotCanvas.height,
+        //     width: snapshotCanvas.width,
+        // }
+        formdata = new FormData()
+        formdata.append('img', imgBlob)
+        formdata.append('height', snapshotCanvas.height)
+        formdata.append('width', snapshotCanvas.width)
+        httpRequest.open('POST', '/face_count/send_img/')
+        httpRequest.setRequestHeader("X-CSRFToken", csrftoken)
+        // httpRequest.setRequestHeader("Content-Type", "");
+        // httpRequest.send(JSON.stringify(json))
+        httpRequest.send(formdata)
         httpRequest.onreadystatechange = function () {
             if (httpRequest.readyState === 4) {
                 if (httpRequest.status === 200) {
@@ -106,10 +171,25 @@ function getJSON() {
                 }
             }
         }
-        httpRequest.open('POST', "/static/face_count.json");
-        httpRequest.send();
     })
 }
+
+// function getJSON() {
+//     return new Promise(function (resolve, reject) {
+//         httpRequest.open('POST', "/static/face_count.json");
+//         httpRequest.send();
+//         httpRequest.onreadystatechange = function () {
+//             if (httpRequest.readyState === 4) {
+//                 if (httpRequest.status === 200) {
+//                     let json = JSON.parse(httpRequest.responseText || 'null')
+//                     resolve(json)
+//                 } else {
+//                     reject()
+//                 }
+//             }
+//         }
+//     })
+// }
 
 function renderCount(data) {
     let face_only_count = null
@@ -136,4 +216,20 @@ function renderStartDateAndTime(unixtime) {
     let dateTime = new Date(unixtime * 1000);
     display_start_date.textContent = dateTime.toLocaleDateString('ja-JP')
     display_start_time.textContent = dateTime.toLocaleTimeString('ja-JP')
+}
+
+function getCookie(cname) {
+    const name = cname + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
