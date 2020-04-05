98eabb476e99e684ef600995071936448380c205
Rei Sugiura <reibomaru@g.ecc.u-tokyo.ac.jp>
2020-04-05 21:49:53 +0900

    modified base.htmlなどなど

--- face_count/urls.py
+++ face_count/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import count
 from .views import usage
+from .views import base
 from django.conf import settings  # 追加
 from django.conf.urls.static import static
 
@@ -8,7 +9,8 @@ urlpatterns = [
     path('', count.index, name='index'),
     path('start_count/', count.count, name='count'),
     path('terminate_count/', count.terminate, name='terminate'),
-    path('usage/', usage.index, name='camera'),
-    path('send_img/', count.analize_img, name='analize_img')
+    path('usage/', usage.index, name='usage'),
+    path('send_img/', count.analize_img, name='analize_img'),
+    path('post_comment/', base.post_comment, name='post_comment')
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
